2b70e1dcfad0e317f65567af5746efc9cca2d1e2
Daniel Hernandez <daniel56hdz@gmail.com>
2017-08-12 09:35:08 -0500

    removed spaces

--- udemy/notes-node/playground/json.js
+++ udemy/notes-node/playground/json.js
@@ -27,7 +27,5 @@ fs.writeFileSync('notes.json', originalNoteString);
 var noteString = fs.readFileSync('notes.json');
 //note
 var note = JSON.parse(noteString);
-console.log(typeof note)
-console.log(note.title)
-
-
+console.log(typeof note);
+console.log(note.title);
