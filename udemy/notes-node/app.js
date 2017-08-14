423baefdd5aff177242ae2be746a28063387283d
Daniel Hernandez <daniel56hdz@gmail.com>
2017-08-13 23:49:45 -0500

    configure yargs and set requirements when entering arguments

--- udemy/notes-node/app.js
+++ udemy/notes-node/app.js
@@ -6,7 +6,32 @@ const yargs = require('yargs');
 //packages made inhouse
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
+//Reuse commands title and body for yargs
+const titleOptions = {
+    describe: 'Title of note', //shows up in the help menu
+    demand: true, //makes sure that it won't run unless the title has been provided
+    alias: 't' //shorthand for --title, node app.js read -t hello 
+};
+const bodyOptions = {
+    describe: 'Body of note',
+    demand: true,
+    alias: 'b'
+};
+
+const argv = yargs
+    .command('add', 'Add a new note', { //this helps us configure yagrs and require arguments
+        title: titleOptions,
+        body: bodyOptions
+    })
+    .command('list', 'List all notes')
+    .command('read', 'Read a note', {
+        title: titleOptions
+    })
+    .command('remove', 'Remove a note', {
+        title: titleOptions
+    })
+    .help()
+    .argv;
 var command = argv._[0];
 
 if(command === 'add') {
