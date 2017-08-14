4202ad5ad45e90c82ddbb17925b66f5294f13b52
Daniel Hernandez <daniel56hdz@gmail.com>
2017-08-14 18:34:39 -0500

    refactor code

--- udemy/weather-app/app.js
+++ udemy/weather-app/app.js
@@ -1,51 +1,20 @@
-// Request allows us to make http calls
-const request = require('request');
-const yargs = require('yargs');
+const yargs = require('yargs'); // Allows us to obtain input from terminal
 
-const argv = yargs
-    .options({
-        a: {
-            demand: true,
-            alias: 'address',
-            describe: 'Address to fetch weather for',
+const geocode = require('./geocode/geocode.js'); // Allows us to retrieve exports from geocode.js 
+
+const argv = yargs //customization to input from terminal 
+    .options({ //options are the flags that we can have when we input
+        a: { //the flag to use when running node
+            demand: true, //this is required, you can't run without an -a flag
+            alias: 'address', //another way to write the same flag, --address
+            describe: 'Address to fetch weather for', //message for help
             string: true //this tells it to always parse the address as a string 
         }
     })
-    .help()
+    .help() //built in help flag --help
     .alias('help', 'h')
     .argv;
-var encodedAddress = encodeURIComponent(argv.address);
-   
-request({
-    // The url is the http call to be made
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address= ${encodedAddress}`,
-    //url: 'https://mapsgleapis.com/maps/api/geocode/json?address=Old%20College,%20South%20Bridge,%20Edinburgh%20EH8%209YL,%20UK',   //To test error parameter on request callback 
-    // this just lets request know that it is infact in json format
-    // allows for it to load in object form 
-    json: true
-}, (error, response, body) => { //this is the callback that runs after the http request is made
-    // The body is the core data that comes back from the server
-    // It is the JSON content from this request
-    // It is not unique to the request module, 
-    // It is part of the http request
-    // printing out the request
-    //console.log(JSON.stringify(body, undefined, 2)); //This pretty prints the object by using stringify
-    //console.log(JSON.stringify(response, undefined, 2)); // This prints the status code (among other things)
-        // the first set of headers are set by google,
-        // the second set of headers is set by us when we set json property to true
-    // The error parameter consist of the errors that occur when the request is made
-    //console.log(JSON.stringify(error, undefined, 2));
-    
-    //Machine Errors, like being unable to connect to a network
-    //Server Errors, like invalid address
-    if (error) {
-        console.log('Unable to connect to Google Servers');
-    } else if (body.status === 'ZERO_RESULTS') {
-        console.log('Unable to find that address');
-    } else if (body.status === 'OK') {
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-    }
-    
-});
+
+
+geocode.geocodeAddress(argv.address);
+//7,20
