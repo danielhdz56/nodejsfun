546f1cfb0f1d28e2987465e2b181b8f5341c6075
Daniel Hernandez <daniel56hdz@gmail.com>
2017-08-14 03:11:52 -0500

    api callback google maps

---
+++ udemy/weather-app/app.js
@@ -0,0 +1,13 @@
+// Request allows us to make http calls
+const request = require('request');
+
+request({
+    // The url is the http call to be made
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1338%20Cross%20Draw%20Dr%20Houston',
+    // this just lets request know that it is infact in json format
+    // allows for it to load in object form 
+    json: true
+}, (error, response, body) => { //this is the callback 
+    // printing out the request
+    console.log(body);
+});
