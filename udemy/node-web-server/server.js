04056a64daf9110f3bb29d785f6447545504ad22
Daniel Hernandez <daniel56hdz@gmail.com>
2017-08-15 17:19:12 -0500

    render templates with some data using handlebars

--- udemy/node-web-server/server.js
+++ udemy/node-web-server/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const hbs = require('hbs'); //espress view engine for handlebars
 
 var app = express();
+
+//This lets us set up some various express related configurations
+//we pass a key value pair
+//the key is view engine
+//the value is hbs
+//this tells express that you want to use the hbs for the view engine
+app.set('view engine', 'hbs');
 //Adding middleware to use express in a different way
 //It is like a third party add on
 //This basically sets us a static directory 
@@ -9,18 +17,19 @@ app.use(express.static(__dirname + '/public'));
 //allows us to set up a handler for our http request
 app.get('/', (req, res) => { //req will contain our headers, res will be what we send back
     // res.send('<h1>Hello Express!</h1>');
-    res.send({
-        name: 'Daniel',
-        likes: [
-            'Chips',
-            'Hiking',
-            'Coding'
-        ]
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Handlebars are awesome',
+        currentYear: new Date().getFullYear()
     });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page');
+    //this allows us to render any of the templates i have set up with my current view engine
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 // bad - send back json with errorMessage 
