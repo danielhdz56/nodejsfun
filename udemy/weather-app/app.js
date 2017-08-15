6de0c3ab501dd42b039bd48bfe5fa07628ddbf9a
Daniel Hernandez <daniel56hdz@gmail.com>
2017-08-15 00:58:36 -0500

    chaining callbacks together for geocode and weather

--- udemy/weather-app/app.js
+++ udemy/weather-app/app.js
@@ -1,43 +1,32 @@
-// const yargs = require('yargs'); // Allows us to obtain input from terminal
+const yargs = require('yargs'); // Allows us to obtain input from terminal
 
-// const geocode = require('./geocode/geocode.js'); // Allows us to retrieve exports from geocode.js 
-
-// const argv = yargs //customization to input from terminal 
-//     .options({ //options are the flags that we can have when we input
-//         a: { //the flag to use when running node
-//             demand: true, //this is required, you can't run without an -a flag
-//             alias: 'address', //another way to write the same flag, --address
-//             describe: 'Address to fetch weather for', //message for help
-//             string: true //this tells it to always parse the address as a string 
-//         }
-//     })
-//     .help() //built in help flag --help
-//     .alias('help', 'h')
-//     .argv;
-
-
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//     if(errorMessage){
-//         console.log(errorMessage);
-//     } else {
-//         console.log(JSON.stringify(results, undefined, 2)); //undefined skips the filter function that we don't need
-//     }
-// });
-// b8df38ec04f30fa4a1ee07f2f7680aea
-
-const request = require('request');
-var geocodeTemp = (latitude, longitude) => {
-    request({
-        url: `https://api.darksky.net/forecast/b8df38ec04f30fa4a1ee07f2f7680aea/${latitude},${longitude}`,
-        json: true
-    }, (error, response, body) => {
-        if(error){
-            console.log('Unable to connect to DarkSky Servers');
-        }else if (response.statusCode === 400){
-            console.log('Unable to fetch weather');
-        }else if (response.statusCode === 200){
-            console.log(body.currently.temperature)            
+const geocode = require('./geocode/geocode.js'); // Allows us to retrieve exports from geocode.js 
+const weather = require('./weather/weather.js');
+const argv = yargs //customization to input from terminal 
+    .options({ //options are the flags that we can have when we input
+        a: { //the flag to use when running node
+            demand: true, //this is required, you can't run without an -a flag
+            alias: 'address', //another way to write the same flag, --address
+            describe: 'Address to fetch weather for', //message for help
+            string: true //this tells it to always parse the address as a string 
         }
     })
-};
-geocodeTemp(29.9486953, -95.4410924);
+    .help() //built in help flag --help
+    .alias('help', 'h')
+    .argv;
+
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(results.address);
+        weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+            if (errorMessage) {
+                console.log(errorMessage);
+            } else {
+                console.log(`It's currently ${weatherResults.temperature}, but it feels like ${weatherResults.apparentTemperature}.`)
+            }
+        });
+    }
+});
