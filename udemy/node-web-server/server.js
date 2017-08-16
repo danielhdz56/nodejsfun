f62c8d5a3e6aac6d223a99d39e11d2fc5e11f97b
Daniel Hernandez <daniel56hdz@gmail.com>
2017-08-16 15:58:11 -0500

    setup start script and heroku port

--- udemy/node-web-server/server.js
+++ udemy/node-web-server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs'); //espress view engine for handlebars
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 //This lets use reuse code from handlebars
@@ -68,6 +69,6 @@ app.get('/bad', (req, res) => {
     });
 })
 //this binds our application to a port on our machine
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
 });
