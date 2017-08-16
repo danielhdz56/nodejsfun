33d38d082920b5a234666520d152a2e843b375d5
Daniel Hernandez <daniel56hdz@gmail.com>
2017-08-16 15:42:10 -0500

    hello test

--- udemy/node-web-server/server.js
+++ udemy/node-web-server/server.js
@@ -21,6 +21,7 @@ app.use((req, res, next) => {
     fs.appendFile('server.log', log + '\n', (err) => {
         if(err) {
             console.log('Unable to append to server.log');
+            console.log('hello');
         }
     });
     next();
