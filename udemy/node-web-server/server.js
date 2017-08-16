f898744890ea8e0899faf0fd149500022ae105f8
Daniel Hernandez <daniel56hdz@gmail.com>
2017-08-16 14:48:25 -0500

    added middleware that appends info about the client making the request

--- udemy/node-web-server/server.js
+++ udemy/node-web-server/server.js
@@ -1,22 +1,43 @@
 const express = require('express');
 const hbs = require('hbs'); //espress view engine for handlebars
+const fs = require('fs');
 
 var app = express();
 
+//This lets use reuse code from handlebars
 hbs.registerPartials(__dirname + '/views/partials');
+
 //This lets us set up some various express related configurations
 //we pass a key value pair
 //the key is view engine
 //the value is hbs
 //this tells express that you want to use the hbs for the view engine
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+//because i didn't call next it will not move on
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
 //Adding middleware to use express in a different way
 //It is like a third party add on
 //This basically sets us a static directory 
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
-    return new Date().getFullYear()
+    return new Date().getFullYear();
 });
 
 hbs.registerHelper('screamIt', (text) => {
