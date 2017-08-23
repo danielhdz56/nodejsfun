b80c2738fb7d02bb0759a26ed564504d4c4c2714
Daniel Hernandez <daniel56hdz@gmail.com>
2017-08-23 18:06:41 -0500

    moved files around

--- bootcamp/day4/instance/app.js
+++ bootcamp/day4/instance/app.js
@@ -6,19 +6,42 @@ function User(firstName, lastName, email) {
         // the constructor was called without "new".
         return new User(firstName, lastName, email);
     }
-    console.log(this)
     this.firstName = firstName,
         this.lastName = lastName,
         this.email = email
 };
 
+function Admin(firstName, lastName, email) {
+    // if (!(this instanceof Admin)) {
+    //     // the constructor was called without "new".
+    //     return new Admin(firstName, lastName, email);
+    // }
+    // User.apply(this, arguments)
+    Admin.prototype = new User();
+    this.admin = true
+};
+
+
+
+
+
+
 
 var daniel = new User('daniel', 'hernandez', 'daniel56hdz@gmail.com');
-var introduce = function(){
-    console.log(this.firstName);
-    console.log(this.lastName);
-    console.log(this.email);
-}
-daniel.introduce = introduce;
-introduce();
-console.log(daniel)
+var alice = new Admin('alice', 'bob', 'someEmail@gmail.com');
+console.log(alice)
+console.log(alice instanceof Admin)
+
+
+// var admin = function(){
+//     return true
+
+// }
+// daniel.admin = admin;
+// var superUser = User('Alice', 'Bob', 'random@email.com');
+// superUser.call(admin)
+// console.log(superUser)
+
+// daniel.introduce = introduce;   
+// introduce();
+// console.log(daniel)
