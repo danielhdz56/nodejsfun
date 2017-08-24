e4f71c2b1014bd42a89d3ed06c81578e85842e23
Daniel Hernandez <daniel56hdz@gmail.com>
2017-08-23 22:27:23 -0500

    add readfile and write file

--- bootcamp/weatherApp/app.js
+++ bootcamp/weatherApp/app.js
@@ -3,11 +3,11 @@
 const userSearch = require('./weather/userSearch');
 
 
-
 //obtain user input 
 const argv = process.argv;
 const name = argv[2];
 var location = argv[3];
 location = encodeURIComponent(location);
 
-userSearch.createUser(name, location);
+//userSearch.createUser(name, location);
+userSearch.WeatherAdmin()
--- bootcamp/weatherApp/weather/userSearch.js
+++ bootcamp/weatherApp/weather/userSearch.js
@@ -1,21 +1,24 @@
 // import libraries
 const weather = require('weather-js');
+const fs = require('fs');
 
-var UserSearch = function (name, location){
+var UserSearch = function (name, location, date) {
     this.name = name,
     this.location = location,
-    this.date = new Date.now();
+    this.date = new Date();
+    this.getWeather = function () {
+        weather.find({
+            search: this.location,
+            degreeType: 'F'
+        }, (err, result) => {
+            if (err) console.log(err);
+            console.log(JSON.stringify(result, null, 2));
+            
+        })
+    };
 };
 
-UserSearch.prototype.getWeather = function(){
-    weather.find({
-        search: this.location, 
-        degreeType: 'F'
-    }, (err, result) => {
-        if(err) console.log(err);
-        console.log(JSON.stringify(result, null, 2));
-    }
-)};
+//UserSearch.prototype.getWeather = 
 
 var createUser = (name, location) => {
     var anonUser = new UserSearch(name, location);
@@ -23,6 +26,51 @@ var createUser = (name, location) => {
     anonUser.getWeather();
 }
 
+
+
+var fetchLogs = () => {
+    try {
+        var logToString = fs.readFileSync('logs.json');
+        return JSON.parse(logToString);
+    } catch (e) {
+        return [];
+    }
+}
+
+var saveLogs = (logs) => {
+    fs.writeFileSync('logs.json', JSON.stringify(logs));
+}
+
+var WeatherAdmin = function(name, location) {
+    this.newUserSearch = new UserSearch(name, location);
+    this.newUserSearch.saveLogs = () => {
+        var logs = fetchLogs();
+        var log = {
+            name,
+            location,
+            date: this.newUserSearch.date
+        };
+        logs.push(log);
+        saveLogs(logs);
+    },
+    this.getData = () => {
+        console.log(fetchLogs()) 
+    }
+};
+// WeatherAdmin.prototype = new UserSearch();
+// WeatherAdmin.prototype.writeData = function () {
+    
+// };
+// WeatherAdmin.prototype.getData = function () {
+//     return fetchLogs();
+// };
+
+
+//var admin = new WeatherAdmin('daniel', 'houston');
+admin.newUserSearch.saveLogs();
+admin.getData();
+
 module.exports = {
-    createUser
+    createUser,
+    WeatherAdmin
 }
